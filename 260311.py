920abd0b3109e268b04b56bac909b824642cc7bc
HelloGaoo <gao13053915898@qq.com>
2026-03-11 19:26:05 +0800

    字体加载 字体文件

--- 260311.py
+++ 260311.py
@@ -1,16 +1,21 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout
+from PyQt5.QtGui import QFontDatabase, QFont
 from qfluentwidgets import (NavigationInterface, NavigationItemPosition, MessageBox,
                             setTheme, Theme, FluentWindow, NavigationAvatarWidget,
                             isDarkTheme, InfoBar, InfoBarPosition)
 from qfluentwidgets import FluentIcon as FIF
 import sys
+import os
 
 class MainWindow(FluentWindow):
     def __init__(self):
         super().__init__()
         
-        # 主题
-        setTheme(Theme.LIGHT)
+        # 加载字体
+        self.loadGlobalFont()
+        
+        # 设置主题
+        setTheme(Theme.DARK)
         
         # 主界面导航
         self.initMainNavigation()
@@ -37,6 +42,27 @@ class MainWindow(FluentWindow):
         setting.setObjectName("setting")
         self.addSubInterface(setting, FIF.SETTING, "设置", NavigationItemPosition.BOTTOM)
         
+    def loadGlobalFont(self):
+        # HarmonyOS Sans
+        font_dir = os.path.join(os.getcwd(), "font", "HarmonyOS_Sans")
+        font_files = [
+            "HarmonyOS_Sans_Thin.ttf",
+            "HarmonyOS_Sans_Light.ttf",
+            "HarmonyOS_Sans_Regular.ttf",
+            "HarmonyOS_Sans_Medium.ttf",
+            "HarmonyOS_Sans_Bold.ttf",
+            "HarmonyOS_Sans_Black.ttf"
+        ]
+        
+        for font_file in font_files:
+            font_path = os.path.join(font_dir, font_file)
+            if os.path.exists(font_path):
+                QFontDatabase.addApplicationFont(font_path)
+        
+        # 默认字体Regular
+        app_font = QFont("HarmonyOS Sans", 10)
+        QApplication.setFont(app_font)
+    
     def moveToCenter(self):
         # 移动窗口到屏幕中央
         desktop = QApplication.desktop().availableGeometry()
